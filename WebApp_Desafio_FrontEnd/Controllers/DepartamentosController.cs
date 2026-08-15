62416946612e46032e43a1b0e682c836f5d0e306
Gabriel Berté <gabriel_._berte95@outlook.com>
2026-08-15 19:03:50 -0300

    feat(departamentos): implementar CRUD completo do módulo de departamentos
    - BackEnd: adiciona ObterDepartamento, GravarDepartamento (insert/update) e ExcluirDepartamento em DepartamentosDAL, expostos via DepartamentosBLL
    - API: cria DepartamentoRequest e adiciona as rotas Obter, Gravar (POST) e Excluir (DELETE) em DepartamentosController
    - FrontEnd: adiciona DepartamentoObter, DepartamentoGravar e DepartamentoExcluir em DepartamentosApiClient
    - FrontEnd: adiciona as actions Cadastrar (GET/POST), Editar (GET) e Excluir (DELETE) em DepartamentosController
    - FrontEnd: cria a view Cadastrar.cshtml e atualiza Listar.cshtml com os botões Adicionar/Editar/Excluir
    - FrontEnd: adiciona Cadastrar.js e atualiza Listar.js com as ações de salvar, cancelar, selecionar e excluir na grid

--- WebApp_Desafio_FrontEnd/Controllers/DepartamentosController.cs
+++ WebApp_Desafio_FrontEnd/Controllers/DepartamentosController.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using WebApp_Desafio_FrontEnd.ApiClients.Desafio_API;
 using WebApp_Desafio_FrontEnd.ViewModels;
+using WebApp_Desafio_FrontEnd.ViewModels.Enums;
 
 namespace WebApp_Desafio_FrontEnd.Controllers
 {
@@ -35,6 +36,79 @@ namespace WebApp_Desafio_FrontEnd.Controllers
                 };
 
                 return Ok(dataTableVM);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseViewModel(ex));
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            var departamentoVM = new DepartamentoViewModel();
+            ViewData["Title"] = "Cadastrar Novo Departamento";
+
+            return View("Cadastrar", departamentoVM);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(DepartamentoViewModel departamentoVM)
+        {
+            try
+            {
+                var departamentosApiClient = new DepartamentosApiClient();
+                var realizadoComSucesso = departamentosApiClient.DepartamentoGravar(departamentoVM);
+
+                if (realizadoComSucesso)
+                    return Ok(new ResponseViewModel(
+                                $"Departamento gravado com sucesso!",
+                                AlertTypes.success,
+                                this.RouteData.Values["controller"].ToString(),
+                                nameof(this.Listar)));
+                else
+                    throw new ApplicationException($"Falha ao gravar o Departamento.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseViewModel(ex));
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Editar([FromRoute] int id)
+        {
+            ViewData["Title"] = "Cadastrar Novo Departamento";
+
+            try
+            {
+                var departamentosApiClient = new DepartamentosApiClient();
+                var departamentoVM = departamentosApiClient.DepartamentoObter(id);
+
+                return View("Cadastrar", departamentoVM);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseViewModel(ex));
+            }
+        }
+
+        [HttpDelete]
+        public IActionResult Excluir([FromRoute] int id)
+        {
+            try
+            {
+                var departamentosApiClient = new DepartamentosApiClient();
+                var realizadoComSucesso = departamentosApiClient.DepartamentoExcluir(id);
+
+                if (realizadoComSucesso)
+                    return Ok(new ResponseViewModel(
+                                $"Departamento {id} excluído com sucesso!",
+                                AlertTypes.success,
+                                "Departamentos",
+                                nameof(Listar)));
+                else
+                    throw new ApplicationException($"Falha ao excluir o Departamento {id}.");
             }
             catch (Exception ex)
             {
